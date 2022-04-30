6a88fd3d7577723c6d4d5b481f389a3f9d9518e4
emmdie <emmanuel.diehl@ifsr.de>
2022-04-30 17:28:23 +0200

    finished exercise 1

--- uebung1.hs
+++ uebung1.hs
@@ -3,5 +3,10 @@ fac 0 = 0
 fac 1 = 1
 fac x = x * fac(x-1)
 
-sumFac :: Int -> Int
-sumFac n m = 
+sumFac :: Int -> Int -> Int
+sumFac n m = sum[fac i|i<-[n..m]]
+
+fibo :: Int -> Int
+fibo 0 = 1
+fibo 1 = 1
+fibo x = (fibo (x-1)) + (fibo (x-2))
